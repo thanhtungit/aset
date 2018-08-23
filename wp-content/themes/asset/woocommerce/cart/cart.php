afd93c57ad6bc2cddfd569c46d147b16f418f7b9
QuangSang <quangsang222@gmail.com>
2018-08-24 01:27:45 +0700

    responsive done

--- wp-content/themes/asset/woocommerce/cart/cart.php
+++ wp-content/themes/asset/woocommerce/cart/cart.php
@@ -51,7 +51,7 @@ do_action( 'woocommerce_before_cart' ); ?>
                     <tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
 
-                        <td class="product-thumbnail">
+                        <td class="product-thumbnail d-sm-block">
                             <?php
                             $thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 
