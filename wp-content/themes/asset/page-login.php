891f2f044e390f1ebfcdc199a1153d6cac12f892
lthanhbinh <lthanhbinh@tma.com.vn>
2018-09-06 19:54:27 +0700

    aaaa

--- wp-content/themes/asset/page-login.php
+++ wp-content/themes/asset/page-login.php
@@ -9,21 +9,21 @@
 		<div class="nav"><?php echo woocommerce_breadcrumb(); ?></div>
 	</div>
 </section>
-<section class="clearfix">
+<section class="clearfix loginPage">
 	<div class="container">
 	   <h1 class="page-title">Login or Create an Account </h1>
 	  <div class="row">
 		 <div class="col-md-6">
-		 	<div class="box">
+		 	<div class="box shadowBox">
 		 		<div class="box-header"><h3>New Customers</h3></div>
 		 		<div class="box-content">
 		 			<p>This is description</p>
-		 			<a href="<?php echo HOME_URL ?>/register/" class="btn btn-primary">Create An Account</a>
+		 			<a href="<?php echo HOME_URL ?>/register/" class="btn-nod32 m-md-auto">Create An Account</a>
 		 		</div>
 		 	</div>
 		 </div>
 		 <div class="col-md-6">
-		 	<div class="box">
+		 	<div class="box shadowBox">
 		 		<?php echo do_shortcode('[woocommerce_my_account]'); ?>
 		 	</div>
 		 </div>
