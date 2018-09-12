8ed7472d0954788f236d0b54dc38398cd97056d6
lthanhbinh <lthanhbinh@tma.com.vn>
2018-09-12 09:29:22 +0700

    aaaaa

---
+++ wp-content/themes/asset/page-compare-product.php
@@ -0,0 +1,286 @@
+<?php get_header(); ?>
+<section class="nav-breadcrumbs clearfix">
+	<div class="container">
+		<div class="nav"><?php echo woocommerce_breadcrumb(); ?></div>
+	</div>
+</section>
+<section class="clearfix <?php echo (is_account_page()) ? 'loginPage' : '' ?> pageCompare">
+	<div class="container">
+		 <div class="row">
+			 <div class="col-md-12">
+				 <h1 class="title-woocommerce">Compare ESET's products</h1>
+			 </div>
+	   </div>
+    </div>
+		<div class="container-fluid nav-controller">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 colScroll">
+						<ul class="nav nav-tabs">
+					    <li class="nav-item">
+					      <a class="nav-link active" data-toggle="tab" href="#windows">
+									<img src="<?php echo TEMPLATE_PATH ?>/images/windows.png" alt="">
+									<span>Windows</span>
+								</a>
+					    </li>
+					    <li class="nav-item">
+					      <a class="nav-link" data-toggle="tab" href="#android">
+									<img src="<?php echo TEMPLATE_PATH ?>/images/android.png" alt="">
+									<span>Android</span>
+					      </a>
+					    </li>
+					    <li class="nav-item">
+					      <a class="nav-link" data-toggle="tab" href="#mac">
+									<img src="<?php echo TEMPLATE_PATH ?>/images/mac.png" alt="">
+									<span>Mac</span>
+					      </a>
+					    </li>
+							<li class="nav-item">
+					      <a class="nav-link" data-toggle="tab" href="#linux">
+									<img src="<?php echo TEMPLATE_PATH ?>/images/linux.png" alt="">
+									<span>Mac</span>
+					      </a>
+					    </li>
+							<li class="nav-item">
+					      <a class="nav-link" data-toggle="tab" href="#multiDevice">
+									<img src="<?php echo TEMPLATE_PATH ?>/images/multi.png" alt="">
+									<span>Multi-Device</span>
+					      </a>
+					    </li>
+					  </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="tab-content col-12 colScroll">
+				  <div class="tab-pane active container" id="windows">
+						<div class="ListImage d-table">
+              <div class="col1 colImage colImageFirst">
+              	<h5></h5>
+              </div>
+              <div class="col2 colImage">
+								<img src="<?php echo TEMPLATE_PATH ?>/images/internet.png" alt="">
+                <span>ESET NOD32 Antivirus</span>
+              </div>
+              <div class="col3 colImage">
+								<img src="<?php echo TEMPLATE_PATH ?>/images/internet.png" alt="">
+                <span>ESET NOD32 Antivirus</span>
+              </div>
+              <div class="col4 colImage">
+								<img src="<?php echo TEMPLATE_PATH ?>/images/internet.png" alt="">
+                <span>ESET NOD32 Antivirus</span>
+              </div>
+							<div class="col4 colImage">
+								<img src="<?php echo TEMPLATE_PATH ?>/images/internet.png" alt="">
+                <span>ESET NOD32 Antivirus</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Antivirus and Antispyware</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Anti-Phishing</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Gamer Mode</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Exploit Blocker</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Cloud-Powered Scanning</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Small System Footprint</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+              <div class="col1 colImage colImageFirst">
+              	<p>Script-Based Attack Protection</p>
+              </div>
+              <div class="col2 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col3 colImage">
+								<span>✓</span>
+              </div>
+              <div class="col4 colImage">
+								<span>✓</span>
+              </div>
+							<div class="col4 colImage">
+								<span>✓</span>
+              </div>
+            </div>
+						<div class="ListImage ListContent d-table">
+            <div class="col1 colImage colImageFirst">
+            	<p>Ransomware Shield</p>
+            </div>
+            <div class="col2 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col3 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col4 colImage">
+							<span>✓</span>
+            </div>
+						<div class="col4 colImage">
+							<span>✓</span>
+            </div>
+          </div>
+					<div class="ListImage ListContent d-table">
+            <div class="col1 colImage colImageFirst">
+            	<p>Free Support in Local Language</p>
+            </div>
+            <div class="col2 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col3 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col4 colImage">
+							<span>✓</span>
+            </div>
+						<div class="col4 colImage">
+							<span>✓</span>
+            </div>
+          </div>
+					<div class="ListImage ListContent d-table">
+            <div class="col1 colImage colImageFirst">
+            	<p>Banking and Payment Protection</p>
+            </div>
+            <div class="col2 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col3 colImage">
+							<span>✓</span>
+            </div>
+            <div class="col4 colImage">
+							<span>✓</span>
+            </div>
+						<div class="col4 colImage">
+							<span>✓</span>
+            </div>
+          </div>
+					<div class="ListImage d-table mt-4 ListBtn">
+						<div class="col1 colImage colImageFirst">
+							<h5></h5>
+						</div>
+						<div class="col2 colImage">
+							<span>Award-winning antivirus<br/> for Windows</span>
+							<div class="group-btn">
+								<a href="#" class="woocommerce-Button button btn-cart btn-green">button</a>
+							</div>
+						</div>
+						<div class="col3 colImage">
+							<span>Award-winning antivirus<br/> for Windows</span>
+							<div class="group-btn">
+								<a href="#" class="woocommerce-Button button btn-cart btn-green">button</a>
+							</div>
+						</div>
+						<div class="col4 colImage">
+							<span>Award-winning antivirus<br/> for Windows</span>
+							<div class="group-btn">
+								<a href="#" class="woocommerce-Button button btn-cart btn-green">button</a>
+							</div>
+						</div>
+						<div class="col4 colImage">
+							<span>Award-winning antivirus<br/> for Windows</span>
+							<div class="group-btn">
+								<a href="#" class="woocommerce-Button button btn-cart btn-green">button</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			  <div class="tab-pane container" id="android">sdfsd</div>
+			  <div class="tab-pane container" id="mac">`3324`</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
